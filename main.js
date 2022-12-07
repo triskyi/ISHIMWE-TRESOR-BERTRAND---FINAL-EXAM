e0232f856a60e331b7a2704a7aa61639174bcd7f
triskyi <112614238+triskyi@users.noreply.github.com>
2022-12-07 09:32:18 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function b(){
+document.getElementById('n').style.color='red';
+}
+
+
+
+/* this to change background color on index page */
+function d(){
+    document.getElementById('dark').style.background='black';
+    document.getElementById('dark').style.color='white';
+
+}
+
+
+function p(){
+    document.getElementById('img').innerHTML.img = 'n.webp';
+
+}
